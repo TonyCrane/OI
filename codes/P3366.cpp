d77d43349903a719658f9f6daaebad75dfe66e68
Tony031218 <975062472@qq.com>
2018-12-25 12:49:36 +0800

    Update

--- codes/P3366.cpp
+++ codes/P3366.cpp
@@ -9,21 +9,22 @@
 using namespace std;
 
 int n, m, ans, _map[5050][5050], dist[5050], pre[5050];
+bool p[5050];
+const int INF = 1e9;
 
 void Prim()
 {
 	int k, min;
-	bool p[5050];
+	dist[1] = 0; p[1] = true;
 	for (int i = 2; i <= n; ++i)
 	{
 		p[i] = false;
 		dist[i] = _map[1][i];
 		pre[i] = 1;
 	}
-	dist[1] = 0; p[1] = true;
 	for (int i = 1; i < n; ++i)
 	{
-		min = INT_MAX;
+		min = INF;
 		k = 0;
 		for (int j = 1; j <= n; ++j)
 		{
@@ -37,7 +38,7 @@ void Prim()
 		p[k] = true;
 		for (int j = 1; j <= n; ++j)
 		{
-			if (!p[j] && _map[k][j] != INT_MAX && dist[j] > _map[k][j])
+			if (!p[j] && _map[k][j] != INF && dist[j] > _map[k][j])
 			{
 				dist[j] = _map[k][j];
 				pre[j] = k;
@@ -51,14 +52,18 @@ int main()
 {
 	scanf("%d %d", &n, &m);
 	for (int i = 1; i <= n; ++i)
-		for (int j = 1; j <= m; ++j)
-			_map[i][j] = INT_MAX;
+		for (int j = 1; j <= n; ++j)
+			_map[i][j] = INF;
 	for (int i = 1; i <= n; ++i) _map[i][i] = 0;
 	for (int i = 1; i <= m; ++i)
 	{
 		int x, y, z;
 		scanf("%d %d %d", &x, &y, &z);
-		_map[x][y] = z;
+		if (_map[x][y] >= z)
+		{
+			_map[x][y] = z;
+			_map[y][x] = z;
+		}
 	}
 	Prim();
 	for (int i = 1; i <= n; ++i)
