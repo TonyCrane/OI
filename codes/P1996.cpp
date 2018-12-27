327afb263a7d33d4dbb7e73a4fdb86a0518e150a
Tony031218 <975062472@qq.com>
2018-12-27 11:51:17 +0800

    Update

--- codes/P1996.cpp
+++ codes/P1996.cpp
@@ -7,8 +7,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int n, m, ans;
+bool vis[210];
+
 int main()
 {
-    
+    scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			if (++ans > n) ans = 1;
+			if (vis[ans]) j--;
+		}
+		printf("%d ", ans);
+		vis[ans] = true;
+	}
     return 0;
 }
