9e5f6b5ca2b1d933de53bb8fd7e728b44a9dbd28
Tony031218 <975062472@qq.com>
2019-04-13 15:43:32 +0800

    Update

--- P5021.cpp
+++ P5021.cpp
@@ -6,131 +6,92 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long LL;
 
 const int inf  = 0x3f3f3f3f;
 const int maxn = 50010;
 
-int n, m, head[maxn], cnt, ans;
+int n, m;
+LL dis;
 
 struct Edge {
-	int to, nxt, val;
-}edges[maxn << 1];
-
+	int from, to, val;
+	Edge(int u, int v, int w) : from(u), to(v), val(w) {}
+};
+vector<Edge> edges;
+vector<int> G[maxn];
 void add(int u, int v, int w) {
-	edges[++cnt].to = v;
-	edges[cnt].val  = w;
-	edges[cnt].nxt  = head[u];
-	head[u] = cnt;
+	edges.push_back(Edge(u, v, w));
+	edges.push_back(Edge(v, u, w));
+	int mm = edges.size();
+	G[u].push_back(mm - 2);
+	G[v].push_back(mm - 1);
 }
 
-namespace Meq1 {
-	int dfs(int now, int fa) {
-		int res1 = 0, res2 = 0;
-		for (int i = head[now]; i; i = edges[i].nxt) {
-			int to = edges[i].to;
-			if (to == fa) continue;
-			res2 = max(res2, dfs(to, now) + edges[i].val);
-			if (res2 > res1) swap(res1, res2);
-		}
-		ans = max(ans, res1 + res2);
-		return res1;
+LL dfs1(int x, int fa) {
+	LL res1 = 0, res2 = 0;
+	for (int i = 0; i < G[x].size(); ++i) {
+		Edge& e = edges[G[x][i]];
+		if (e.to == fa) continue;
+		res2 = max(res2, dfs1(e.to, x) + e.val);
+		if (res2 > res1) swap(res1, res2);
 	}
-
-	void solve() {
-		dfs(1, 0);
-		printf("%d\n", ans);
-		return ;
-	}
+	dis = max(dis, res1 + res2);
+	return res1;
 }
 
-namespace Aeq1 {
-	int arr[maxn];
-	
-	bool cmp(int a, int b) {
-		return a > b;
-	}
-
-	void solve() {
-		for (int i = head[1]; i; i = edges[i].nxt) {
-			int to = edges[i].to;
-			arr[to - 1] = edges[i].val;
-		}
-		sort(arr + 1, arr + n, cmp);
-		ans = inf;
-		for (int i = 1; i <= m; ++i) {
-			ans = min(ans, arr[i] + arr[2 * m - i + 1]);
-		}
-		printf("%d\n", ans);
-		return ;
-	}
-}
-
-namespace OneChain {
-	int arr[maxn], ans;
-
-	int Dfs(int now, int fa) {
-		int res1 = 0, res2 = 0;
-		for (int i = head[now]; i; i = edges[i].nxt) {
-			int to = edges[i].to;
-			if (to == fa) continue;
-			res2 = max(res2, Dfs(to, now) + edges[i].val);
-			if (res2 > res1) swap(res1, res2);
-		}
-		ans = max(ans, res1 + res2);
-		return res1;
-	}
-
-	void dfs(int now, int fa) {
-		for (int i = head[now]; i; i = edges[i].nxt) {
-			int to = edges[i].to;
-			if (to == fa) continue;
-			dfs(to, now);
-			arr[now] = edges[i].val;
-		}
-	}
-
-	bool judge(int x) {
-		int t = 0, now = 0;
-		for (int i = 1; i < n; ++i) {
-			if (now + arr[i] >= x) {
-				now = 0;
-				t++;
+LL dfs(int u, int fa, LL& len, LL lim) {
+	int ans = 0;
+	multiset<long long> s;
+	printf("size: %d\n", G[u].size());
+	for (int i = 0; i < G[u].size(); ++i) {
+		Edge& e = edges[G[u][i]];
+		printf("e.to: %d\n", e.to);
+		if (e.to == fa) {
+			LL res = 0;
+			ans += dfs(e.to, u, res, lim);
+			res += e.val;
+			if (res >= lim) {
+				ans++;
 			} else {
-				now += arr[i];
+				s.insert(res);
 			}
 		}
-		return t >= m;
 	}
-
-	void solve() {
-		dfs(1, 0);
-		Dfs(1, 0);
-		int l = 1, r = ans, mid;
-		while (l < r) {
-			mid = l + r + 1 >> 1;
-			if (judge(mid)) l = mid;
-			else r = mid - 1;
+	len = 0;
+	while (!s.empty()) {
+		LL res = *s.begin();
+		s.erase(s.begin());
+		multiset<long long>::iterator it = s.lower_bound(lim - res);
+		if (it != s.end()) {
+			s.erase(it);
+			ans++;
+		} else {
+			len = max(len, res);
 		}
-		printf("%d\n", l);
-		return ;
 	}
+	return ans;
 }
 
 int main() {
-	bool aie1 = true;
 	scanf("%d %d", &n, &m);
 	for (int i = 1; i < n; ++i) {
 		int a, b, l;
 		scanf("%d %d %d", &a, &b, &l);
-		add(a, b, l); add(b, a, l);
-		if (a != 1) aie1 = false;
+		add(a, b, l);
 	}
-	if (m == 1) {
-		Meq1::solve();
-	} else if (aie1) {
-		Aeq1::solve();
-	} else {
-		OneChain::solve();
+	dfs1(1, 0);
+	LL l = 0, r = dis + 1, mid;
+	while (r - l > 1) {
+		mid = l + r >> 1;
+		LL res = 0;
+		printf("dfs: %lld\n", dfs(1, 0, res, mid));
+		if (dfs(1, 0, res, mid) >= m) {
+			l = mid;
+		} else {
+			r = mid;
+		}
 	}
+	printf("%lld\n", l);
 	return 0;
 }
