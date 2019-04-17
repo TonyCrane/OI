c731c902ce0f7d4e7c890a5c0943b6556c72640a
Tony031218 <975062472@qq.com>
2019-04-17 16:04:38 +0800

    Update

---
+++ NOIp2017/codes/P3951.cpp
@@ -0,0 +1,16 @@
+/*************************************************************
+ *  > File Name        : P3951.cpp
+ *  > Author           : Tony
+ *  > Created Time     : 2019年04月17日 星期三 16时02分50秒
+**************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long a, b;
+
+int main() {
+	scanf("%lld %lld", &a, &b);
+	printf("%lld\n", a * b - a - b);
+	return 0;
+}
