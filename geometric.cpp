2dc1d49fbf14533efed963f6d89c74533dd62eab
Tony031218 <975062472@qq.com>
2019-04-24 14:54:29 +0800

    Update

--- geometric.cpp
+++ geometric.cpp
@@ -173,6 +173,48 @@ int getTangents(Circle A, Circle B, Point* a, Point* b) {
     return cnt;
 }
 
+typedef vector<Point> Polygon;
+int isPointInPolygon(Point p, Polygon poly) {
+    int wn = 0;
+    int n = poly.size();
+    for (int i = 0; i < n; ++i) {
+        if (OnSegment(p, poly[i], poly[(i + 1) % n])) return -1;
+        int k = dcmp(Cross(poly[(i + 1) % n] - poly[i], p - poly[i]));
+        int d1 = dcmp(poly[i].y - p.y);
+        int d2 = dcmp(poly[(i + 1) % n].y - p.y);
+        if (k > 0 && d1 <= 0 && d2 > 0) wn++;
+        if (k < 0 && d2 <= 0 && d1 > 0) wn--;
+    }
+    if (wn != 0) return 1;
+    return 0;
+}
+Polygon ConvexHull(vector<Point> p) {
+    sort(p.begin(), p.end());
+    p.erase(unique(p.begin(), p.end()), p.end());
+    int n = p.size();
+    int m = 0;
+    Polygon ch(n + 1);
+    for (int i = 0; i < n; ++i) {
+        while (m > 1 && Cross(ch[m - 1] + ch[m - 2], p[i] - ch[m - 2]) <= 0) m--;
+        ch[m++] = p[i];
+    }
+    int k = m;
+    for (int i = n - 2; i >= 0; --i) {
+        while (m > k && Cross(ch[m - 1] + ch[m - 2], p[i] - ch[m - 2]) <= 0) m--;
+        ch[m++] = p[i];
+    }
+    if (n > 1) m--;
+    ch.resize(m);
+    return ch;
+}
+double PolygonArea(Polygon p) {
+    double area = 0;
+    int n = p.size();
+    for(int i = 1; i < n-1; i++)
+        area += Cross(p[i] - p[0], p[i + 1] - p[0]);
+    return area / 2;
+}
+
 int main() {
     return 0;
 }
